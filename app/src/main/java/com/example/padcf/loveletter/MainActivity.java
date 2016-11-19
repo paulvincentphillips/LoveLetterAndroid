49081a8c4e82b945958cd70d7fb61ab0e5ece727
padcf <pid.flynn@gmail.com>
2016-11-19 22:34:27 +0000

    Second update for tonight.
    You can run the app, reveal and hide the cards.
    
    You can also click on the cads which will toast telling you you clicked on it.
    
    Jesus it took a lot of messing about...

--- app/src/main/java/com/example/padcf/loveletter/MainActivity.java
+++ app/src/main/java/com/example/padcf/loveletter/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,29 +20,73 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView image1 = (ImageView) findViewById(R.id.imageView1);
-        final ImageView image2 = (ImageView) findViewById(R.id.imageView2);
+        //instantiate your image buttons here for use
+        final ImageButton ib = (ImageButton) findViewById(R.id.imageButton);
+        final ImageButton ib2 = (ImageButton) findViewById(R.id.imageButton2);
+
+
+        //instatiate your toggle button here
         final ToggleButton mainButton = (ToggleButton) findViewById(R.id.toggleButton);
 
-        image1.setVisibility(View.INVISIBLE);
-        image2.setVisibility(View.INVISIBLE);
+        //set the images to be invisible on start
+        ib.setVisibility(View.INVISIBLE);
+        ib2.setVisibility(View.INVISIBLE);
 
+
+        //call the onListner method to do stuff when yohu click on an image button, pass in the two images
+        addListnerOnButton(ib, ib2);
+
+
+        //this method sets the images visible or invisible when clicked...this is the toggle button
         mainButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked)
                 {
-                    image1.setVisibility(View.VISIBLE);
-                    image2.setVisibility(View.VISIBLE);
+                    ib.setVisibility(View.VISIBLE);
+                    ib2.setVisibility(View.VISIBLE);
+                    Toast.makeText(getApplicationContext(),"Choose a card", Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
-                    image1.setVisibility(View.INVISIBLE);
-                    image2.setVisibility(View.INVISIBLE);
+                    ib.setVisibility(View.INVISIBLE);
+                    ib2.setVisibility(View.INVISIBLE);
                 }
+
+
 
             }
         });
+
+
+
+
+
+
+    }
+
+    //this method let's you do stuff when you click on a button
+    public void addListnerOnButton(View ib, View ib2)
+    {
+
+        //here we decide what happens when you click on either image. each image is passed in as a formal parameter, ib or ib2
+        ib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "You pressed button ONE", Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+
+        ib2.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  Toast.makeText(getApplicationContext(), "You pressed button TWO", Toast.LENGTH_SHORT).show();
+              }
+        }
+        );
+
+
 
 
     }
