8ce78add0acfecb8d4d9b4f8a84aef31b5d81ab9
Conor Brady <bradyc12@tcd.ie>
2016-12-12 21:27:43 +0000

    Changed some backgrounds and layouts

--- app/src/main/java/com/example/padcf/loveletter/MainActivity.java
+++ app/src/main/java/com/example/padcf/loveletter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.padcf.loveletter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
--- app/src/main/java/com/example/padcf/loveletter/Menu.java
+++ app/src/main/java/com/example/padcf/loveletter/Menu.java
@@ -1,6 +1,7 @@
 package com.example.padcf.loveletter;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -21,10 +22,12 @@ public class Menu extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.menu_screen);
 
 
-        //Declaring the imagebuttons used on the menu screen-PlayGame, Tutorial
 
+        final MediaPlayer buttonSound = MediaPlayer.create(this,R.raw.button);
+
+        //Declaring the imagebuttons used on the menu screen-PlayGame, Tutorial
         ImageButton PlayGame = (ImageButton) findViewById(R.id.play);
-        ImageButton videotutorial = (ImageButton) findViewById(R.id.tutorial);
+        ImageButton videotutorial = (ImageButton) this.findViewById(R.id.tutorial);
 
 
         PlayGame.setOnClickListener(this);
@@ -35,11 +38,13 @@ public class Menu extends AppCompatActivity implements View.OnClickListener {
     //By clicking the game button on the Menu screen, it will begin a new game from the MainActivity class.
     @Override
     public void onClick(View view) {
+
         switch (view.getId()) {
             case R.id.play:
                 Intent intent = new Intent(Menu.this, MainActivity.class);
                 startActivity(intent);
                 break;
+
      //By clicking the tutorial button on the Menu screen, it will redirect to the below youtube tutorial.
             case R.id.tutorial:
                 Intent webintent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=6XHpHrKtIEM&t=502s"));
